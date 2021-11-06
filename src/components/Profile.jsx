800eafb233c523958b820ef80c8280ad720d6459
Nikita <givemeblood57@gmail.com>
2021-11-06 16:01:53 +0300

    realized first components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <img src="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg"></img>
+      <div>
+        avatar+description
+      </div>
+        <div>
+        My posts
+        <div>
+          New
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
