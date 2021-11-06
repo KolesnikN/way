5251775878b44f4f00bd9e6bede074e00165b496
Nikita <givemeblood57@gmail.com>
2021-11-06 17:09:50 +0300

    add css modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import cssClass from './Navbar.module.css'
 
 const Navbar = () => {
-  return (<nav className="nav">
-    <div>
+  return (<nav className={cssClass.nav}>
+    <div className={cssClass.item}>
       <a>Profile</a>
-    </div><div>
+    </div>
+    <div className={`${cssClass.item} ${cssClass.active}`}>
       <a>Messages</a>
-    </div><div>
+    </div>
+    <div className={cssClass.item}>
       <a>Music</a>
-    </div><div>
+    </div>
+    <div className={cssClass.item}>
       <a>News</a>
-    </div><div>а
+    </div>
+    <div className={cssClass.item}>
       <a>Settings</a>
     </div>
   </nav>)
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import cssClass from './Profile.module.css'
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={cssClass.content}>
       <img src="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg"></img>
       <div>
         avatar+description
@@ -12,11 +13,11 @@ const Profile = () => {
         <div>
           New
         </div>
-        <div>
-          <div>
+        <div className='posts'>
+          <div className={cssClass.item}>
             post 1
           </div>
-          <div>
+          <div className={cssClass.item}>
             post 2
           </div>
         </div>
