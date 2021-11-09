39fc9800435ad9a98718403f9a152d13c31817d3
nick <givemeblood57@gmail.com>
2021-11-09 10:32:59 +0300

    add props usage in message and little refactor the code

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,18 @@
+import MyPosts from './MyPosts/MyPosts';
+import React from 'react'
+import cssClass from './Profile.module.css'
+
+const Profile = () => {
+    return (
+        <div>
+            <img
+                src="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg"></img>
+            <div>
+                avatar+description
+            </div>
+            <MyPosts/>
+        </div>
+    )
+}
+
+export default Profile;
