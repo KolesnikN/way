78e2462d782868e6c247c663fb447222300c0628
nick <givemeblood57@gmail.com>
2021-11-09 09:01:42 +0300

    add messages menu

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,27 @@ import cssClass from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={cssClass.dialogs}>
+            <div className={cssClass.dialogsItems}>
+                <div className={cssClass.dialogsItems + ' ' + cssClass.active}>
+                    Nick
+                </div>
+                <div className={cssClass.dialogsItems}>
+                    Uliana
+                </div>
+                <div className={cssClass.dialogsItems}>
+                    Vova
+                </div>
+                <div className={cssClass.dialogsItems}>
+                    Serega
+                </div>
+            </div>
+            <div className={cssClass.messages}>
+                <div className={cssClass.messages}>privet</div>
+                <div className={cssClass.messages}>4e delaew</div>
+                <div className={cssClass.messages}>a? a?</div>
+                <div className={cssClass.messages}>Pos</div>
+            </div>
         </div>
     )
 }
