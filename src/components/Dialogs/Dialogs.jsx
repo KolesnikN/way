583c582089e7e9b33153682713185dd6e8b99cad
nick <givemeblood57@gmail.com>
2021-11-10 06:20:55 +0300

    props for messages and posts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -15,20 +15,19 @@ const Dialog = (props) => {
     )
 }
 
+let dialogData = [{id: 1, name: "Nick"}, {id: 2, name: "Uliana"}]
+let messageData = [{id: 1, message: "Kak dela"}, {id: 2, message: "4to delaew"}]
+
 const Dialogs = (props) => {
     return (
         <div className={cssClass.dialogs}>
             <div className={cssClass.dialogsItems}>
-                <Dialog name="Nick" id="1"/>
-                <Dialog name="Uliana" id="2"/>
-                <Dialog name="Serega" id="3"/>
-                <Dialog name="Dima" id="4"/>
+                <Dialog id={dialogData[0].id} name={dialogData[0].name}/>
+                <Dialog id={dialogData[1].id} name={dialogData[1].name}/>
             </div>
             <div className={cssClass.messages}>
-                <Message message="Kak dela"/>
-                <Message message="4to delaew"/>
-                <Message message="When we can meet?"/>
-                <Message message="Say him NO"/>
+                <Message message={messageData[0].message}/>
+                <Message message={messageData[1].message}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,8 @@ import Post from './Post/Post'
 import React from 'react'
 import cssClass from './MyPosts.module.css'
 
+let postData = [{id: 1, message: "Privet kak dela to?", likes: 23}, {id: 2, message: "4e delaew", likes: 4}]
+
 const MyPosts = () => {
   return (
     <div className={cssClass.postBlock}>
@@ -13,8 +15,8 @@ const MyPosts = () => {
         <button className={cssClass.addPost}>Add post</button>
       </div>
       <div className='posts'>
-        <Post message={"Privet kak dela to?"} likes={"2"}/>
-        <Post message={"4e delaew"} likes={"3"}/>
+        <Post message={postData[0].message} likes={postData[0].likes}/>
+        <Post message={postData[1].message} likes={postData[1].likes}/>
       </div>
     </div>
   )
