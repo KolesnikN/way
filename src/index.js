6c3d4ed6f1e22def3a968b7230cba7b18a633b5d
nikitakolesnik-deriv <nikita.kolesnik@regentmarkets.com>
2021-11-29 12:10:53 +0300

    add context.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import store from "./Redux/state"
+import { Provider } from "./context"
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-      <App
-        state={store.getState()}
-        dispatch={store.dispatch.bind(store)}
-        store={store}
-      />
+      <Provider>
+        <App
+          state={store.getState()}
+          dispatch={store.dispatch.bind(store)}
+          store={store}
+        />
+      </Provider>
     </BrowserRouter>,
     document.getElementById("root")
   )
