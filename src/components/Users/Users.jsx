582943973d9ffde9bdf10f6369815f4096dfec77
nikitakolesnik-deriv <givemeblood57@gmail.com>
2021-12-01 10:09:26 +0300

    make pure function of axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,17 +4,19 @@ import * as axios from "axios"
 import avaImg from "./avatar/userAvatar.png"
 
 let Users = (props) => {
-  if (props.users.length === 0) {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {
-        props.setUsers(response.data.items)
-      })
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(response.data.items)
+        })
+    }
   }
-  debugger
 
   return (
     <div>
+      <button onClick={getUsers}>Get users</button>
       {props.users.map((user) => (
         <div key={user.id}>
           <span>
