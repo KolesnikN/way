e0abbc393ef15e20c764ec5e075767a484260f52
KolesnikN <givemeblood57@gmail.com>
2021-12-14 12:32:44 +0300

    add componentDidMount to update status effect

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -20,6 +20,14 @@ class ProfileContainer extends React.Component {
     this.props.getStatus(userId)
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({
+        status: this.props.status,
+      })
+    }
+  }
+
   render() {
     return (
       <Profile
